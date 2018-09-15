8bb04d6d281803644aa9964403e9552cf458cb81
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-15 04:26:30 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2018_09_15_040025) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
 end
